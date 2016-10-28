248f7aeff53742bad216124e2fb036c203b933ce
Hemang Rajyaguru <hemdagem@gmail.com>
2016-10-28 21:16:31 +0100

    Add project files.

---
+++ google-starred-locations-to-json/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace google_starred_locations_to_json
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
